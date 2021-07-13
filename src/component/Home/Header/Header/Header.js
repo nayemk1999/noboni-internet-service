cb4f246517e91caf132125efb01e90fbe24fd61e
Nayem Khan <nayemk900@gmail.com>
2021-07-14 03:06:04 +0600

    merge add new branch

--- src/component/Home/Header/Header/Header.js
+++ src/component/Home/Header/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner from '../../Banner/Banner';
-import NavBar from '../NavBar/NavBar'
 import './Header.css'
+import NavBar from '../NavBar/Navbar'
 
 const Header = () => {
     return (
